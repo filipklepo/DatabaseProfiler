8188697fe017dd27fb5e377df2860936465a72ca
Filip Klepo <filipklepo@gmail.com>
2017-06-05 01:36:23 +0200

    Finish regular expression rule functionality

--- src/main/java/hr/fer/zavrad/dbprofiler/controller/rule/RegularExpressionController.java
+++
@@ -1,7 +0,0 @@
-package hr.fer.zavrad.dbprofiler.controller.rule;
-
-/**
- * Created by filip on 04.06.17..
- */
-public class RegularExpressionController {
-}
---
+++ src/main/java/hr/fer/zavrad/dbprofiler/controller/rule/RegularExpressionRuleController.java
@@ -0,0 +1,43 @@
+package hr.fer.zavrad.dbprofiler.controller.rule;
+
+import hr.fer.zavrad.dbprofiler.model.rule.RegularExpressionRule;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.sql.Connection;
+
+public class RegularExpressionController {
+
+    @FXML
+    private TextField tfRegularExpression;
+    @FXML
+    private TextField tfTable;
+    @FXML
+    private TextField tfColumn;
+    @FXML
+    private ListView lvResult;
+    @FXML
+    private Button btnExecute;
+
+    private final Connection connection;
+
+    public RegularExpressionController(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void initialize() {
+        btnExecute.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                lvResult.getItems().clear();
+                new RegularExpressionRule(
+                        connection, tfRegularExpression.getText(), tfTable.getText(), tfColumn.getText())
+                        .execute(lvResult);
+            }
+        });
+    }
+}
